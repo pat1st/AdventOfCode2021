33fd919d872fb3002a38af65de7125f0333eafbc
Speidel, Patrick (Avnet) <Patrick.Speidel@avnet.eu>
2021-12-01 11:29:18 +0100

    Add project files.

---
+++ AdventOfCode2021/Dec1st.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    public class Dec1st
+    {
+        public int GetIncreaseCount(string measurements)
+        {
+            if (measurements.Trim().Length < 1)
+            {
+                return 0;
+            }
+            var splitted = measurements.Split(',');
+            var counter = 0;
+            var previous = -1;
+            foreach (var item in splitted)
+            {
+                int actual = int.Parse(item);
+                if (previous == -1)
+                {
+                    previous = actual;
+                    continue;
+                }
+                if (actual > previous)
+                {
+                    counter++;
+                }
+                previous = actual;
+            }
+
+            return counter;
+        }
+
+        public int GetSlidingIncrease(string measurements)
+        {
+            if (measurements.Trim().Length < 6)
+            {
+                return 0;
+            }
+            var splitted = measurements.Split(',');
+            if (splitted.Length < 4)
+            {
+                return 0;
+            }
+            var newMeasurements = "";
+
+            for (int i = 0; i < splitted.Length - 2; i++)
+            {
+                var sum = int.Parse(splitted[i]) + int.Parse(splitted[i + 1]) + int.Parse(splitted[i + 2]);
+                newMeasurements += sum.ToString() + ",";
+            }
+
+            return GetIncreaseCount(newMeasurements.Trim(','));
+        }
+    }
+}
---
+++ AdventOfCode2021Tests/Dec1stTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode2021;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Tests
+{
+    [TestClass()]
+    public class Dec1stTests
+    {
+        [TestMethod()]
+        public void GetIncreaseCountTest_EmptyString_Returns_0()
+        {
+            Dec1st dec1st = new Dec1st();
+            var result = dec1st.GetIncreaseCount("");
+            var expected = 0;
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void GetIncreaseCountTest_SingleValue_Returns_0()
+        {
+            Dec1st dec1st = new Dec1st();
+            var result = dec1st.GetIncreaseCount("125");
+            var expected = 0;
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        public void GetIncreaseCountTest_TwoValuesSecondHigher_Returns_1()
+        {
+            Dec1st dec1st = new Dec1st();
+            var result = dec1st.GetIncreaseCount("125, 126");
+            var expected = 1;
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        public void GetIncreaseCountTest_TwoValuesSecondLower_Returns_0()
+        {
+            Dec1st dec1st = new Dec1st();
+            var result = dec1st.GetIncreaseCount("125, 124");
+            var expected = 0;
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        public void GetIncreaseCountTest_SampleOfPuzzle_Returns_7()
+        {
+            Dec1st dec1st = new Dec1st();
+            var result = dec1st.GetIncreaseCount("199, 200, 208, 210, 200, 207, 240, 269, 260, 263");
+            var expected = 7;
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void GetSlidingIncreaseTest_TwoValuesOnly_Returns_0()
+        {
+            Dec1st dec1st = new Dec1st();
+            var result = dec1st.GetSlidingIncrease("199, 200");
+            var expected = 0;
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        public void GetSlidingIncreaseTest_FourIncreasingValues_Returns_1()
+        {
+            Dec1st dec1st = new Dec1st();
+            var result = dec1st.GetSlidingIncrease("1,2,3,4");
+            var expected = 1;
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
