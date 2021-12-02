660ccb7c5f3638b141241a45b699481f8fcdad6b
Speidel, Patrick (Avnet) <Patrick.Speidel@avnet.eu>
2021-12-02 15:16:54 +0100

    finished day 2

--- Python/Dec2nd/Dec2nd.py
+++ Python/Dec2nd/Dec2nd.py
@@ -3,7 +3,8 @@ import io
 input = "Python\Dec2nd\input"   # "Python\Dec2nd\sample"
 with open(input) as f:
     lines = f.readlines()
-depth = horiz = 0
+
+depth = horiz = aim = 0
 
 for line in lines:
     (dir, val) = line.split()
@@ -14,5 +15,22 @@ for line in lines:
             depth -= int(val)
         case "down":
             depth += int(val)
+
+print(depth, horiz, depth*horiz)
+
+depth = horiz = aim = 0
+
+for line in lines:
+    (dir, val) = line.split()
+    match dir:
+        case "forward":
+            horiz += int(val)
+            depth += aim*int(val)
+        case "up":
+            #depth -= int(val)
+            aim -= int(val)
+        case "down":
+            #depth += int(val)
+            aim += int(val)
 
 print(depth, horiz, depth*horiz)
