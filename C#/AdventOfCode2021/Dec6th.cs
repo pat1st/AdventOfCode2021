c9dc029b520d20ff40dcc8a7243b727758a1b4bc
Patrick Speidel <pat1st@gmx.de>
2021-12-07 00:15:59 +0100

    Dec 6th Part2 not finished yet, out of memory issues.. need to find different approach

--- C#/AdventOfCode2021/Dec6th.cs
+++ C#/AdventOfCode2021/Dec6th.cs
@@ -13,27 +13,83 @@ namespace AdventOfCode2021
         {
             Fish = new List<int>();
         }
+        static int maxCount = 200000;
 
         public int PredictSwarmSize(List<int> fish, int days)
-        {            
+        {
+            List<List<int>> fishTanks = distributeFishToTanks(fish);
+
+            
+            bool reDistribution = false;
+
             for (int i = 0; i < days; i++)
             {
-                for (int fishCounter = 0; fishCounter < fish.Count; fishCounter++)
+                foreach (var fishTank in fishTanks)
                 {
-                    var actualFish = fish[fishCounter];
-                    if (actualFish == 0)
-                    {                        
-                        fish[fishCounter] = 6;
-                        fish.Add(9);
-                    }
-                    else
+                    for (int fishCounter = 0; fishCounter < fishTank.Count; fishCounter++)
                     {
-                        fish[fishCounter]--;
+                        fishMakeFishies(fishTank, fishCounter);
                     }
+                    if (fishTank.Count > maxCount)
+                    {
+                        reDistribution = true;
+                    }
+                    Console.Write(fishTank.Count + " ");
                 }
-                //Console.WriteLine(string.Join(",", fish));
+                
+                if (reDistribution)
+                {
+                    Console.WriteLine("redist..");
+                    List<List<int>> SubFishTanks = distributeFishToTanks(fish);
+                    foreach (var item in fishTanks)
+                    {
+                        SubFishTanks.AddRange(distributeFishToTanks(item));
+                    }
+                    fishTanks = SubFishTanks;
+                    reDistribution = false;
+                }
+
+                Console.WriteLine(i.ToString() + ": ----------" + Environment.NewLine); // + fishTanks[0].Count + " " + fishTanks[1].Count);
+                Console.WriteLine(countFishTotal(fishTanks));
+
             }
-            return fish.Count;
+            return int.Parse(countFishTotal(fishTanks));
+        }
+
+        private static List<List<int>> distributeFishToTanks(List<int> fish)
+        {
+            var fishTanks = new List<List<int>>(maxCount);
+            foreach (var singleFish in fish)
+            {
+                fishTanks.Add(new List<int> { singleFish });
+            }
+
+            return fishTanks;
+        }
+
+        private string countFishTotal(List<List<int>> fishTanks)
+        {
+            int i = 0;
+            foreach (var item in fishTanks)
+            {
+                i += item.Count;
+            }
+            return i.ToString();
+        }
+
+        private static void fishMakeFishies(List<int> fish, int fishCounter)
+        {
+            var actualFish = fish[fishCounter];
+            if (actualFish == 0)
+            {
+                fish[fishCounter] = 6;
+                fish.Add(9);
+            }
+            else
+            {
+                fish[fishCounter]--;
+            }
+            
         }
     }
 }
