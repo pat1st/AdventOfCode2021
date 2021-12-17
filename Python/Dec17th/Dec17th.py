7a613a1e020bfc3ce1def9127510cf5095408bb5
Patrick Speidel <pat1st@gmx.de>
2021-12-17 21:54:54 +0100

    Dec17th step 2 done

--- Python/Dec17th/Dec17th.py
+++ Python/Dec17th/Dec17th.py
@@ -13,6 +13,8 @@ vx = 0
 vy = 0
 maxy = 0
 
+targetCount = []
+
 
 def checkIfTargetReached(px, py):
     if (tx1 <= px <= tx2) or (tx2 <= px <= tx1):
@@ -36,7 +38,9 @@ def checkIfBeyondTarget(px, py):
     return False
 
 
-def oneLoop(vx, vy, draw):
+def oneLoop(vx, vy, draw, targetCount):
+    saveX = vx
+    saveY = vy
     done = False
     px = 0
     py = 0
@@ -53,6 +57,7 @@ def oneLoop(vx, vy, draw):
         target = checkIfTargetReached(px, py)
         if target == True:
             done = True
+            targetCount.append([saveX, saveY])
             #print('Target area reached')
             #print('maxY: ', maxy)
 
@@ -68,23 +73,25 @@ def oneLoop(vx, vy, draw):
                 vx += 1
         vy -= 1
 
-    return maxy, target, beyond
+    return maxy, target, beyond, targetCount
 
 
 maxPair = [0, 0]
 maxAbs = 0
 
-for i in range(0, 150):
-    for j in range(0, 150):
+for i in range(-300, 300):
+    for j in range(-300, 300):
         #print('------- ', i, j)
-        maxy, target, beyond = oneLoop(i, j, False)
+        maxy, target, beyond, targetCount = oneLoop(i, j, False, targetCount)
         if target and maxy > maxAbs:
             maxAbs = maxy
-            print(maxAbs, i, j)
+            print(maxAbs, i, j, len(targetCount))
             maxPair = [i, j]
 
 print('maximum height: ', maxAbs)
 print('x', maxPair[0], 'y', maxPair[1])
+print('targetcount', len(targetCount))
+print('targetcount', targetCount)
 
 plt.axhline(y=0, linestyle='-', color='grey')
 plt.axvline(x=0, linestyle='-', color='grey')
@@ -92,7 +99,7 @@ fig, ax = plt.subplots()
 ax.add_patch(Rectangle((tx1, ty1), tx2-tx1, ty2-ty1))
 ax.text(maxPair[0]+2, maxAbs-1, 'max: ' + str(maxAbs), fontsize=12)
 
-oneLoop(maxPair[0], maxPair[1], True)
+oneLoop(maxPair[0], maxPair[1], True, [])
 
 plt.grid()
 plt.show()
